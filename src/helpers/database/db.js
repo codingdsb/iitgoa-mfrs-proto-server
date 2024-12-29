88dde36f163d9bfb4bc34f9f266194be0e069a96
codingdsb <codingdsb@gmail.com>
2024-12-29 20:07:58 +0530

    made envs

--- src/helpers/database/db.js
+++ src/helpers/database/db.js
@@ -1,8 +1,10 @@
-const mysql = require("mysql")
+const mysql = require("mysql");
+require("dotenv").config();
 const db = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"password",
-    database:"mess_frs",
-})
-module.exports = db
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+module.exports = db;
